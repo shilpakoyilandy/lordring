47d1955f97e6e885b452003ac331d997a12dc14e
shilpacredot <shilpa.s@credot.dev>
2023-10-06 09:47:24 +0530

    detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,17 @@
-import SearchForm from "./components/SearchForm";
-import { charactorList } from "./hooks/charactor";
-import Table from "./components/Table";
-import { useState } from "react";
-import Pagination from "./components/Pagination";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Details from "./pages/Details";
 function App() {
-  const [pageNumber, setPageNumber] = useState(1);
-  const [formField, setFormField] = useState({
-    name: "",
-    race: "",
-    gender: "",
-    sort: "",
-  });
-  const { data } = charactorList(pageNumber, 10, formField);
-  const handlePreviousClick = () => {
-    if (pageNumber > 1) {
-      setPageNumber(pageNumber - 1);
-    }
-  };
-
-  const handleNextClick = () => {
-    if (pageNumber < data?.pages) {
-      setPageNumber(pageNumber + 1);
-    }
-  };
+  
   return (
     <>
       <Routes>
+      <Route path="/" element={<Home/>} />
+        
         <Route path="/customer/:id" element={<Details />} />
       </Routes>
-      <SearchForm formField={formField} setFormField={setFormField} />
-      <Table data={data} />
-      <div>
-        <Pagination
-          pageNumber={pageNumber}
-          setPageNumber={setPageNumber}
-          totalPage={data?.pages}
-          handlePreviousClick={handlePreviousClick}
-          handleNextClick={handleNextClick}
-        />
-      </div>
+     
+      
     </>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Pagination from "./Pagination";
-
+import { Link } from "react-router-dom";
 export default function Table({ data }) {
   return (
     <div className="container mx-auto ring ring-gray-100 border-inherit pt-3">
@@ -21,7 +21,7 @@ export default function Table({ data }) {
               <td>{item?.name}</td>
               <td>{item?.race}</td>
               <td>{item?.gender}</td>
-              <td>View More</td>
+              <td><Link to={`/customer/${item?._id}`} >View More</Link></td>
             </tr>
           ))}
         </tbody>
